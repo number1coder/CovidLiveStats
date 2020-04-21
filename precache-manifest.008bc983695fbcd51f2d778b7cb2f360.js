e8c566a0c9a689a55fa0ca796e4f93af5e0b3499
Tofunmi <TTdude@Ifeolus-MacBook-Pro-2.local>
2020-04-21 15:47:21 +0100

    Updates

---
+++ precache-manifest.008bc983695fbcd51f2d778b7cb2f360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aef00cfa5d932351084722d71cabb1b",
+    "url": "/CovidLiveStats/index.html"
+  },
+  {
+    "revision": "c6d941761f0599c31ac7",
+    "url": "/CovidLiveStats/static/css/main.e3e70dc7.chunk.css"
+  },
+  {
+    "revision": "e48010fe944ca316bacf",
+    "url": "/CovidLiveStats/static/js/2.c1ddd02e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidLiveStats/static/js/2.c1ddd02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d941761f0599c31ac7",
+    "url": "/CovidLiveStats/static/js/main.d14ada82.chunk.js"
+  },
+  {
+    "revision": "0332152044ed26386064",
+    "url": "/CovidLiveStats/static/js/runtime-main.02c99417.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidLiveStats/static/media/image.d7265326.png"
+  }
+]);
